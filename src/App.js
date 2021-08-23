3addf446db9d2ffb3b3e5d68b0bdd97b9e39a3b5
msolorio <msolorio.github@gmail.com>
2021-08-23 11:02:05 -0700

    Rendering todos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    inputVal: ''
+    inputVal: '',
+    todoList: ['crush some code', 'make a dope app']
   }
 
   // event is an object with information about the typing event
@@ -14,12 +15,52 @@ class App extends React.Component {
     this.setState({ inputVal: event.target.value });
   }
 
+  // We use an arrow function becuase this function is triggered by a DOM event (submitting the form)
+  addTodo = (event) => {
+    // Stop the page from refreshing (browser's default behavior)
+    event.preventDefault();
+
+    // Note: We never want to update state directly. Only through calling 'setState()'
+    // this.state.todoList.push(this.state.inputVal);
+
+    // Create a new array with our existing todoList and combining it with the new todo
+    const updatedList = [
+      ...this.state.todoList,
+      this.state.inputVal
+    ];
+
+    this.setState({ todoList: updatedList });
+  }
+
+  // Does not need to be an arrow function because it is not triggered by a DOM event (clicking, submitting a form, typing into input, etc...)
+
+  // Converts the array of todos into an array of JSX
+  renderTodos() {
+    // ['crush some code', 'make a dope app']
+    
+    // forEach method
+    const todosListJSX = [];
+    
+    this.state.todoList.forEach((todo) => {
+      const todoJSX = <li>{todo}</li>;
+      
+      todosListJSX.push(todoJSX);
+    });
+    
+    // [<li>crush some code</li>, <li>make a dope app</li>]
+
+    return todosListJSX;
+  }
+
+
   render() {
+    this.renderTodos();
+
     return (
       <div className="App">
         <h1>React Todo List</h1>
   
-        <form>
+        <form onSubmit={this.addTodo}>
           <input 
             type="text" 
             placeholder="Enter a Todo..." 
@@ -29,6 +70,10 @@ class App extends React.Component {
   
           <input type="submit" value="Add Todo" />
         </form>
+        
+        <ul>
+          {this.renderTodos()}
+        </ul>
       </div>
     );
   }
